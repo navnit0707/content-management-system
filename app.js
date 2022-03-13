4af3648b0bbc781d762aadd4f3ed13a9b4689063
navnit0707 <knavnit81@gmail.com>
2022-03-13 21:08:43 +0530

    added css which will statically render and about page with dynamic render will the help of templating handlebars

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
 
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.engine('handlebars', exphbs.engine({defaultLayout : 'home'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/',(req,res)=>{
+    res.render('home/index');
+});
+app.get('/about',(req,res)=>{
+    res.render('home/about');
+});
 app.listen(4500,()=>{
     console.log(`listening on port 4500`);
 });
