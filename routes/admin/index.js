62bd3a5566b3a36263a361dd0a806628c613b979
navnit0707 <knavnit81@gmail.com>
2022-03-14 01:49:31 +0530

    dashboard handlebar and re assigning default layout value

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const req = require('express/lib/request');
 const router = express.Router();
+
+router.all('/*',(req,res,next)=>{
+    req.app.locals.layout ='admin';
+
+    next();
+});
 
 router.get('/',(req,res)=>{
     res.render('admin/index');
