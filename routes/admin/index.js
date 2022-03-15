9ee058c948c53ed5bf1617417f1a594a1bc5f608
navnit0707 <knavnit81@gmail.com>
2022-03-15 19:46:27 +0530

    moving forward with error 1

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const faker = require('faker');
+const Post = require('../../models/Post');
 
 router.all('/*', (req, res, next) => {
     req.app.locals.layout = 'admin';
@@ -8,7 +10,27 @@ router.all('/*', (req, res, next) => {
 });
 
 router.get('/', (req, res) => {
+
+
     res.render('admin/index');
+});
+
+router.post('/generate-fake-posts', (req, res) => {
+    let numb = parseInt(req.body.amount);
+    for (let i = 0; i < numb; i++) {
+
+        let post = new Post();
+        post.title = faker.name.title();
+        post.status = 'public';
+        post.allowComments = faker.random.boolean();
+        post.body = faker.lorem.sentence();
+
+        post.save(function(err) {
+            if (err) { throw err; }
+
+        });
+        res.redirect('/admin/posts');
+    }
 });
 
 
