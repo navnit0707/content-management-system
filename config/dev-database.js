b7385c039e00164520035e1ebd0b1a4bd9b5678c
navnit0707 <knavnit81@gmail.com>
2022-03-20 09:11:12 +0530

    mongoconnection

--- config/dev-database.js
+++ config/dev-database.js
@@ -1,5 +1,4 @@
 module.exports = {
 
-    mongoDbUrl: 'mongodb://localhost/cms'
-
+    mongoDbUrl: 'mongodb+srv://navnit070701:IzHWYC9a3gtbqe3K@cms.no0vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
